900bdc52ce423365483838e97dd8a9df408be880
Darlan Thiago <tdarlan1@hotmail.com>
2021-08-06 20:53:59 -0300

    Create Consume External Service Trait

---
+++ app/Services/Traits/ConsumeExternalService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+trait ConsumeExternalService
+{
+    public function headers(
+        array $headers = [],
+    ) {
+
+        $defaultHeaders = [
+            'Accept' => 'application/json',
+            'Authorization' => $this->token,
+        ];
+
+        return array_merge($defaultHeaders, $headers);
+    }
+
+    /**
+     * @return \Illuminate\Http\Response
+     */
+
+    public function request(
+        string $method,
+        string $endPoint,
+        array $formParams = [],
+        array $headers = []
+    ) {
+
+        return Http::withHeaders($this->headers($headers))
+            ->$method($endPoint, $formParams);
+    }
+}
